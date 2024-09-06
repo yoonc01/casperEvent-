76e5d1febf8a7730dc3072ad7f4c4f2a823c4e76
Hyojun Yoon <hyoyoon@cx2r8s4.42seoul.kr>
2024-09-06 14:37:26 +0900

    remove svgr that use svg file as a component

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
-import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   resolve: {
     alias: [
       { find: '@', replacement: resolve(__dirname, 'src') },
